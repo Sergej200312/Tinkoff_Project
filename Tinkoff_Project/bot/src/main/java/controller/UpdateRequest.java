898d06972cb657b271695dd897a8f39a62e81661
Sergej200312 <sergej-mixajlov-2003@bk.ru>
2023-03-28 20:43:49 +0500

    Реализовал контроллеры в модуле bot

---
+++ Tinkoff_Project/bot/src/main/java/controller/UpdateRequest.java
@@ -0,0 +1,10 @@
+package controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateRequest {
+    private String message;
+}
---
+++ Tinkoff_Project/bot/src/main/java/controller/UpdateResponse.java
@@ -0,0 +1,11 @@
+package controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateResponse {
+    private Long id;
+    private String message;
+}
